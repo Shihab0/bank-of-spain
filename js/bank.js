e4d216d5a888dfd2b1916bc5d1a8cfb10716b9aa
Shihab0 <ataharshihab1@gmail.com>
2023-06-21 04:02:39 +0600

    tltal balance set successfully

--- js/bank.js
+++ js/bank.js
@@ -23,6 +23,10 @@ depositBtn.addEventListener("click", () => {
   );
   const currentDepositBalance = previousDepositBalance + depositValue;
   depositBalance.innerText = currentDepositBalance;
+  //total balance
+  const previousTotalBalance = +parseFloat(totalBalance.innerText).toFixed(2);
+  const currentTotalBalance = previousTotalBalance + depositValue;
+  totalBalance.innerText = currentTotalBalance;
 });
 
 withdrawBtn.addEventListener("click", () => {
@@ -38,8 +42,18 @@ withdrawBtn.addEventListener("click", () => {
   const previousWithdrewBalance = +parseFloat(
     withdrawBalance.innerText
   ).toFixed(2);
+  //   const currentWithdrawBalance = previousWithdrewBalance + withdrawValue;
+  //   withdrawBalance.innerText = currentWithdrawBalance;
+
+  //total balance
+  const previousTotalBalance = +parseFloat(totalBalance.innerText).toFixed(2);
+  const currentTotalBalance = previousTotalBalance - withdrawValue;
+  console.log(currentTotalBalance);
+  if (currentTotalBalance < 0) {
+    alert("You have not enough Money!!");
+    return;
+  }
+  totalBalance.innerText = currentTotalBalance;
   const currentWithdrawBalance = previousWithdrewBalance + withdrawValue;
   withdrawBalance.innerText = currentWithdrawBalance;
 });
-
-//total balance
